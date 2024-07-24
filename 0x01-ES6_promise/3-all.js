01b19a102e7c94fd5c6d1fbb6cff49d65076623b
El-sharqawy <elsharqawy2@gmail.com>
2024-07-25 02:55:57 +0300

    Promise Task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+import { import uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+	return Promise.all([uploadPhoto(), createUser()])
+		.then((response) => {
+			console.log(`${response[0].body} ${response[1].firstName ${response[1].lastnName}}`);
+		})
+		.catch(() => console.log('Signup system offline'));
+}
