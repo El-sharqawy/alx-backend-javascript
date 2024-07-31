98a0f6d296c7cb879a17c30d7dda35769fb80d49
El-sharqawy <elsharqawy2@gmail.com>
2024-07-31 03:02:01 +0300

    Task8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, string) {
   let cleanSet = '';
-  if (!string || typeof (string) !== 'string') { return result; }
+  if (!string || typeof (string) !== 'string') { return cleanSet; }
   for (const s of set) {
     if (s && s.startsWith(string)) {
       cleanSet += `${s.slice(string.length)}-`;
